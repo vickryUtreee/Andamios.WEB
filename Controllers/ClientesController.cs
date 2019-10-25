90847010cb5ea6f91b7e474beb2f4e402037b23a
Vickry <vickry.santana@gmail.com>
2019-10-25 11:39:08 -0400

    Controlador Cliente Creado. Vista Index de los clientes creada

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAndamios.Controllers
+{
+    public class ClientesController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Ver(int id)
+        {
+            return View();
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+ 
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+ 
+    }
+}
