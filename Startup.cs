d813ed8eaeb4c9cc520288f4408daf45140d0f0e
Vickry <vickry.santana@utreee.com>
2019-11-14 16:23:26 -0400

    Sync

--- Startup.cs
+++ Startup.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Andamios.DOMAIN;
+using Andamios.DOMAIN.Context;
+using Andamios.DOMAIN.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.IISIntegration;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -26,6 +31,9 @@ namespace ProyectoAndamios
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AndamiosDominicanosContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("AndamiosDominicanosContext")));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -33,10 +41,45 @@ namespace ProyectoAndamios
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddDefaultIdentity<Usuario>(options => options.SignIn.RequireConfirmedEmail = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<AndamiosDominicanosContext>();
+
+
+            services.Configure<IdentityOptions>(options => {
+
+                // Password Settings
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredLength = 6;
+
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
             
-
             //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 
         }
@@ -55,25 +98,19 @@ namespace ProyectoAndamios
                 app.UseHsts();
             }
 
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            app.UseAuthentication();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "areas",
                     template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
-                //routes.MapAreaRoute(
-                //    name: "Clientes",
-                //    areaName: "Clientes",
-                //    template: "Cliente/{controller=Home}/{action=Index}/{id?}");
-
-                //routes.MapAreaRoute(
-                //    name: "Contratos",
-                //    areaName: "Contratos",
-                //    template: "Contratos/{controller=Home}/{action=Index}/{id?}");
 
                 routes.MapRoute(
                     name: "default",
